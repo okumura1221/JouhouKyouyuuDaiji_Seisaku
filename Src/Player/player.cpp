386b8417210b278785be2a5500fd7d2f19919f20
okumura1221 <165745160+okumura1221@users.noreply.github.com>
2024-04-19 01:56:42 +0900

    確認用文字だし！

--- Src/Player/player.cpp
+++ Src/Player/player.cpp
@@ -35,6 +35,8 @@ void Player::Init() {
 	//プレイヤー画像読み込み
 	playerHan = LoadGraph(PLAYER_PATH);
 	mouseHan = LoadGraph(MOUSE_PATH);
+
+	speed = 0;
 }
 
 void Player::Step() {
@@ -91,14 +93,10 @@ void Player::Draw() {
 	DrawFormatString(0, 0, GetColor(255, 0, 0), 
 		"プレイヤー座標:X:%f\nプレイヤー座標:Y:%f\nプレイヤー座標:Z:%f"
 		, m_pos.x, m_pos.y, m_pos.z);
-	DrawFormatString(0, 55, GetColor(0, 255, 0),
+	DrawFormatString(0, 55, GetColor(0, 0, 255),
 		"マウス座標:X:%f\nマウス座標:Y:%f\nマウス座標:Z:%f"
 		, mouse_pos.x, mouse_pos.y, mouse_pos.z);
-	DrawFormatString(0, 110, GetColor(0, 0, 255),
-		"m_move_vec座標:X:%f\nm_move_vec座標:Y:%f\nm_move_vec座標:Z:%f"
-		, m_move_vec.x, m_move_vec.y, m_move_vec.z);
-
-	DrawFormatString(0, 165, GetColor(255, 0, 255),
+	DrawFormatString(0, 110, GetColor(255, 0, 255),
 		"加速度:%f", speed);
 }
 
