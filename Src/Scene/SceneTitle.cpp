7d9813d9bb668c2bd969614fbeff77c0f3d318fb
sawa51 <shogo151219@icloud.com>
2024-04-21 20:47:50 +0900

    タイトル追加

--- Src/Scene/SceneTitle.cpp
+++ Src/Scene/SceneTitle.cpp
@@ -4,8 +4,15 @@
 
 SceneTitle::SceneTitle() 
 {
+	SetMouseDispFlag(true);
+
 	// タイトル背景ハンドル
 	TitleBGHandle = 0;
+
+	TitleButtonHandle1 = 0;
+	TitleButtonHandle2 = 0;
+
+	//TitleSoundPath = 0;
 }
 SceneTitle::~SceneTitle() { FinTitle(); }
 
@@ -15,6 +22,14 @@ void SceneTitle::InitTitle()
 	// タイトル背景ハンドル
 	TitleBGHandle = LoadGraph(TITLE_BG_PATH);
 
+	//タイトルボタンハンドル
+	TitleButtonHandle1 = LoadGraph(TITLE_BUTTTON_PATH1);
+	TitleButtonHandle2 = LoadGraph(TITLE_BUTTTON_PATH2);
+
+	//サウンドハンドル
+	TitleSoundPath = LoadSoundMem(TITLE_BGM_PATH);
+
+	PlaySoundMem(TitleSoundPath, DX_PLAYTYPE_BACK, true);
 	// タイトルループへ
 	g_CurrentSceneID = SCENE_ID_LOOP_TITLE;
 }
@@ -22,9 +37,21 @@ void SceneTitle::InitTitle()
 // タイトル通常処理
 void SceneTitle::StepTitle()
 {
+	GetMousePoint(&mouseX, &mouseY);
+
+
+	
 
 	// Enterを押したら
-	if(InputKey::Push(KEY_INPUT_RETURN))
+	//if(InputKey::Push(KEY_INPUT_RETURN))
+	//{
+	//	// タイトルシーンを終了
+	//	FinTitle();
+	//}
+
+	//タイトルボタンを押したら
+	if (mouseX>=273&&mouseX<=1022&&mouseY >= 588&&mouseY<=675&&
+		(GetMouseInput() & MOUSE_INPUT_LEFT) != 0)
 	{
 		// タイトルシーンを終了
 		FinTitle();
@@ -36,6 +63,21 @@ void SceneTitle::DrawTitle()
 {
 	// タイトル背景描画
 	DrawGraph(0, 0, TitleBGHandle, true);
+
+	//タイトルボタン描画
+	if (mouseX >= 273 && mouseX <= 1022 && mouseY >= 588 && mouseY <= 675)
+	{
+	   DrawGraph(0, 0, TitleButtonHandle2, true);
+	}
+	else
+	{
+		DrawGraph(-4, -4, TitleButtonHandle1, true);
+	}
+
+	//座標確認用
+   /* DrawFormatString(0, 55, GetColor(0, 0, 255),
+		"マウス座標:X:%d\nマウス座標:Y:%d"
+		, mouseX, mouseY);*/
 }
 
 // タイトル終了処理
@@ -43,6 +85,13 @@ void SceneTitle::FinTitle()
 {
 	// タイトル背景ハンドル
 	DeleteGraph(TitleBGHandle);
+
+	//タイトルボタンハンドル
+	DeleteGraph(TitleButtonHandle1);
+	DeleteGraph(TitleButtonHandle2);
+
+	//BGMハンドル
+	DeleteSoundMem(TitleSoundPath);
 
 	// プレイシーンに移動
 	g_CurrentSceneID = SCENE_ID_INIT_PLAY;
--- Src/Scene/SceneTitle.h
+++ Src/Scene/SceneTitle.h
@@ -2,17 +2,27 @@
 
 // 画像パス
 // タイトル背景
-#define TITLE_BG_PATH "Data/Image/BackGround/TentativeTitleBG.png"
+#define TITLE_BG_PATH "Data/Image/Title/TitleImage.png"
+
+#define TITLE_BUTTTON_PATH1 "Data/Image/Title/SelectButton.png"
+#define TITLE_BUTTTON_PATH2 "Data/Image/Title/SelectButton2.png"
+
+//タイトルBGM
+#define TITLE_BGM_PATH "Data/Sound/TitleSceneBGM.mp3"
 
 class SceneTitle {
 private:
 	// タイトル背景ハンドル
 	int TitleBGHandle;
+	int TitleButtonHandle1, TitleButtonHandle2;
 
+	int mouseX, mouseY;
+
+	int TitleSoundPath;
 public:
 	SceneTitle();
 	~SceneTitle();
-
+	
 	// タイトルの初期化
 	void InitTitle();
 
